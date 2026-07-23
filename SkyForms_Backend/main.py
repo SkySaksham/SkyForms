2ccaa8454dc7b7c576ee04cd1996e400e66c1011
SkySaksham <skysaksham1608@gmail.com>
2026-07-24 02:33:12 +0530

    fastapi basics

--- SkyForms_Backend/main.py
+++ SkyForms_Backend/main.py
@@ -3,3 +3,15 @@
 
 
 
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def home():
+    return {"message": "App Running !!"}
+
+@app.get("/multiply")
+def multiply(a:float,b:float):  
+        return {"result" :a*b}
+    
